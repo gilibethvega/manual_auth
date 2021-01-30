327615ba3972903b6e51e696eefa5c5981ef6539
Gilibeth Vega <gilibethvega@gmail.com>
2021-01-30 17:44:17 -0300

    se incopora modelo user y rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only [:new, :create, :show] 
+
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
