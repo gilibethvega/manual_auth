ea0cdcc1012122ce607b3b059e15984b8e0334c8
Gilibeth Vega <gilibethvega@gmail.com>
2021-01-30 19:19:43 -0300

    se agrega form para crear usuario y se configuran controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Usuario registrado Correctamente"
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(session[:user_id])
+  end
+  private
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
