f3e387f2834a6d5a91aece76fb995512ae6160fd
Gilibeth Vega <gilibethvega@gmail.com>
2021-01-30 21:04:46 -0300

    se completa incoporaciones de sesiones y de relación story con user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logeado correctamente'
+    else
+      flash.now[:notice] = 'Email o Password Inválido'
+      render action: :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Sign Out successfully'
+  end
+
+  private
+  def sessions_params
+    params.require(:sessions).permit(:email, :password)
+  end
+end
